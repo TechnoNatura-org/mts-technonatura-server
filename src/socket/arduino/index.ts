9ba79e1fa9df686baf831463445ce6a188481049
MTs Technonatura <80556134+mts-technonatura@users.noreply.github.com>
2021-06-23 15:22:48 +0700

    Update index.ts

--- src/socket/arduino/index.ts
+++ src/socket/arduino/index.ts
@@ -91,7 +91,8 @@ export default function ArduinoSocket(req: Request, socketGlobal: Socket) {
                   sensorId: sensorId,
                   data: realtimedata.data,
                   dateAdded: realtimedata.date,
-                  id: realtimedata.id
+                  // @ts-ignore
+                  id: realtimedata._id
                 });
             }
           }
